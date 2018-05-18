7a866349f90f15c8f00e9281d54bba9182f70f17
Alex Bieliakov <bieliakov.alex@gmail.com>
2018-05-18 20:45:58 +0300

    Добавлена демонстрация алгоритма all-of

--- Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <list>
 #include <conio.h>
 
 using namespace std;
@@ -14,7 +16,13 @@ struct OddElements
 
 int main()
 {
+	//vector<int> v({ 1,2,3,4,5,6,7,8 });
+	//vector<int> v({ 2,4,6,8,10 });
+	list<int> v({ 2,4,6,8,10 });
 	//all-of
+	cout << (all_of(v.begin(), v.end(), OddElements()) ? "All elements are odd": "Not all elements are odd") << endl;
+	
+
 	_getch();
 	return 0;
 }
